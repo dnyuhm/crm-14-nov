c18b0e66b31b821e6d1f2a1b960c612ac4f62c2a
dnyuhm <dylanmeuriot@gmail.com>
2022-11-14 17:00:53 +0100

    Commit 10 : router angular & lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', redirectTo: 'orders', pathMatch: 'full' }];
+const routes: Routes = [
+  { path: '', redirectTo: 'orders', pathMatch: 'full' },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
